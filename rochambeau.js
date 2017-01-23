350070d1cb91ec8f36d5106b3b780ed9e8c35669
Andrea D Martinez <am21325@srcs.loc>
2017-01-23 08:05:11 -0800

    changes

--- rochambeau.js
+++ rochambeau.js
@@ -1,105 +1,97 @@
-// This is the varaible that stores the score.
-// score[0] = wins, score[1] = ties, score[2] = losses
-var score = [0,0,0];
-
-// The variables store the current player's and computer's choices
-// 0 = Rock, 1 = Paper, 2 = Scissors
+var score = [0, 0, 0];
+var lastScore = [0, 0, 0];
+var Countmatch = 1;
+var matches = [0, 0, 0];
 var playerChoice;
 var computerChoice;
 
-function playGame(){
-    // Here is the game ruleset algorithm
+function playGame() {
     if (playerChoice == computerChoice) {
-        // We have a tie!
-        console.log("tie");
         return 0;
-    } else if (playerChoice == 0 && computerChoice == 2) {
-        // Rock beats scissors - a win!
-        console.log("win");
-        return 1;
-    } else if (playerChoice == 0 && computerChoice == 4) {
-        // Rock beats Lizard - a win!
-        console.log("win");
-        return 1;
-    }else if (playerChoice == 1 && computerChoice == 1) {
-        // Paper beats Rock - a win!
-        console.log("win");
-        return 1;
-    }else if (playerChoice == 1 && computerChoice == 3) {
-        // Paper beats Spock - a win!
-        console.log("win");
-        return 1;
-    }else if (playerChoice == 2 && computerChoice == 1) {
-        // Scissors beats paper - a win!
-        console.log("win");
-        return 1;
-    }else if (playerChoice == 2 && computerChoice == 4) {
-        // Scissors beats lizard - a win!
-        console.log("win");
-        return 1;
-    }else if (playerChoice == 3 && computerChoice == 2) {
-        // Spock beats scissors - a win!
-        console.log("win");
-        return 1;
-    }else if (playerChoice == 3 && computerChoice == 0) {
-        // Spock beats rock - a win!
-        console.log("win");
-        return 1;
-    }else if (playerChoice == 4 && computerChoice == 3) {
-        // Lizard beats Spock - a win!
-        console.log("win");
-        return 1;
-    } else if (playerChoice == 4 && computerChoice == 1) {
-        // Lizard beats paper - a win!
-        console.log("win");
-        return 1;
-    } else {
-        // All other combinations are losses
-        console.log("lose");
-        return -1;
     }
+    else if (playerChoice == 0) {
+        if (computerChoice == 2 || computerChoice == 4) {
+            return 1;
+        }
+    }
+    else if (playerChoice == 1) {
+        if (computerChoice == 0 || computerChoice == 3) {
+            return 1;
+        }
+    }
+    else if (playerChoice == 2) {
+        if (computerChoice == 1 || computerChoice == 4) {
+            return 1;
+        }
+    }
+    else if (playerChoice == 3) {
+        // Win if player picks Spock and computer picks rock or scissors.
+        if (computerChoice == 0 || computerChoice == 2) {
+            return 1;
+        }
+    }
+    else if (playerChoice == 4) {
+        // Win if player picks lizard and computer picks Spock or paper.
+        if (computerChoice == 1 || computerChoice == 3) {
+            return 1;
+        }
+    }
+    return -1;
 }
 
-function displayScoreBoard(winsId, lossesId, tiesId){
+function displayScoreBoard(winsId, lossesId, tiesId, matchesWonID, matchesLostID, matchesTiedID) {
     document.getElementById(winsId).innerHTML = score[0];
     document.getElementById(lossesId).innerHTML = score[2];
     document.getElementById(tiesId).innerHTML = score[1];
+    document.getElementById(matchesWonID).innerHTML = matches[0];
+    document.getElementById(matchesLostID).innerHTML = matches[2];
+    document.getElementById(matchesTiedID).innerHTML = matches[1];
 }
 
-function updateScore(val){
-    ++score[val];
-    console.log("The score is now " + score);
+function updateScore(array, index) {
+    array[index]++;
 }
 
-function displayGameResult(resultId){
-    // Define an array of text labels for the choices 0, 1, 2;
-    var choices = ["Rock", "Paper", "Scissors", "Spock", "Lizard"];
-    // Now play the game and store the result
+function displayGameResult(resultId) {
+    var choices = ["rock", "paper", "scissors", "Spock", "lizard"];
     var result = playGame();
-    // Create a message for the player
-    var message = "Your choice was " + choices[playerChoice] + " and the computer's choice was " + choices[computerChoice] + "<br/>";
-    // Add to the base message if it was a win, loss, or tie
-    if (result == 1) {
-        // Display that it was a win
-        updateScore(0);
-        document.getElementById(resultId).innerHTML = message + "YOU WIN!";
+    var message = "Your choice was " + choices[playerChoice] + " and the computer's choice was " + choices[computerChoice] + ". <br/>";.
+    if(result == 1) {
+        updateScore(score, 0);
+        document.getElementById(resultId).innerHTML = message + "You win! :)";
         document.getElementById(resultId).className = "alert alert-success";
-    } else if (result == -1) {
-        updateScore(2);
-        // Display that it was a loss
-        document.getElementById(resultId).innerHTML = message + "YOU LOSE! ";
+    }
+    else if (result == -1) {
+        updateScore(score, 2);.
+        document.getElementById(resultId).innerHTML = message + "You lose! :(";
         document.getElementById(resultId).className = "alert alert-danger";
-    } else {
-        // Display that it was a tie
-        updateScore(1);
-        document.getElementById(resultId).innerHTML = message + "A tie. ";
+    }
+    else {
+        updateScore(score, 1);
+        document.getElementById(resultId).innerHTML = message + "A tie. :|";
         document.getElementById(resultId).className = "alert alert-info";
+    }
+    Countmatch++;
+    if (matchCount == 4) {
+        if (score[0] - lastScore[0] > score[2] - lastScoree[2]) {
+            updateScore(matches, 0);
+        }
+        else if (score[0] - lastScore[0] < score[2] - lastScore[2]) {
+            updateScore(matches, 2);
+        }
+        else {
+            updateScore(matches, 1);
+        }
+        // Set previous score to score.
+        for (var i = 0; i < score.length; i++) {
+            lastScoree[i] = score[i];
+        }.
+        Countmatch = 1;
     }
 }
 
 function storePlayerChoice(choice) {
     playerChoice = choice;
-    console.log("My choice = " + playerChoice);
     storeComputerChoice();
 }
 
@@ -109,3 +101,6 @@ function storeComputerChoice() {
     console.log("Computer choice = " + computerChoice);
 }
 
+    // Generate computer's random choice.
+    computerChoice = Math.floor(Math.random() * 5);
+}
